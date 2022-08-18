f561b5e9a54c7b6a8b19502f867689a6c748d109
José Carlos <josecarlosbsilva.17@gmail.com>
2022-08-18 18:43:32 -0300

    add sequence function on attributes of Task's factory bot file

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
     status { 0 }
-    title { "MyString" }
-    description { "MyString" }
+    sequence(:title) {|n| "MyString#{n}" }
+    sequence(:description) {|n| "MyString#{n}" }
   end
 end
