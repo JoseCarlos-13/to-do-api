f76b45a6ebefcabc9b4953e0011930079071a343
José Carlos <josecarlosbsilva.17@gmail.com>
2022-08-19 14:05:00 -0300

    add the enum numbers for each label in the 'task' model test file

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe Task, type: :model do
   it { is_expected.to validate_presence_of(:description) }
 
   it { is_expected.to define_enum_for(:status) }
-  it { is_expected.to define_enum_for(:status).with_values([:in_development, :done]) }
+  it { is_expected.to define_enum_for(:status).with_values(in_development: 0, done: 1) }
 end
