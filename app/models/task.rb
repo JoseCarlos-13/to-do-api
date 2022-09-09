e0b663e1cfef8ede05dc070287d4d6e4b59043d7
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-09-09 17:06:06 -0300

    correct some files with literal string comment
    add a specific comment line for to correct a error reported by rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
--- app/serializers/tasks/index/tasks_serializer.rb
+++ app/serializers/tasks/index/tasks_serializer.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Tasks::Index::TasksSerializer < ActiveModel::Serializer
   attributes :id, :title, :status
 end
