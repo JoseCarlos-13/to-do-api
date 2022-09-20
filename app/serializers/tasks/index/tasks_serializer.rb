92580d6618b7dd9df732c8d37e26c43ebdcbcfc5
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-09-20 01:07:41 -0300

    add module in task_serializer

--- app/serializers/tasks/index/tasks_serializer.rb
+++ app/serializers/tasks/index/tasks_serializer.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class Foo::Bar
-  class Tasks::Index::TasksSerializer < ActiveModel::Serializer
-    attributes :id, :title, :status
+class Foo
+  class Bar
+    class Tasks::Index::TasksSerializer < ActiveModel::Serializer
+      attributes :id, :title, :status
+    end
   end
 end
