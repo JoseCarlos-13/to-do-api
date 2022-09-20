6e2b7ad12a7c26b04bef46b31c1e21b90cb68b10
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-09-20 01:20:06 -0300

    add variable in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,8 +65,9 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config = config.integrate
   Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
+    config do |with|
       with.test_framework :rspec
       with.library :rails
     end
