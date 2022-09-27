c04abda659b30d7e5ed889ba4aa29502772d293d
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-09-27 15:26:05 -0300

    add index text context with pagination

--- spec/requests/tasks_request_spec.rb
+++ spec/requests/tasks_request_spec.rb
@@ -21,5 +21,23 @@ RSpec.describe 'Tasks', type: :request do
         expect(json_body[0]).to have_key(:status)
       end
     end
+
+    context "when have pagination included" do
+      let(:tasks) { create_list(:task, 6) }
+
+      before do
+        tasks
+
+        get '/tasks', params: { page: 1, items: 2 }
+      end
+
+      it 'return' do
+        expect(response).to have_http_status(:ok)
+        expect(json_body[0]).to have_key(:id)
+        expect(json_body[0]).to have_key(:title)
+        expect(json_body[0]).to have_key(:status)
+        expect(json_body.count).to eq(2)
+      end
+    end
   end
 end
