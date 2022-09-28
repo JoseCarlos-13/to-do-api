da2ee67083ef9b9a035951603b44ad8db815aaf2
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-09-27 22:04:25 -0300

    add index text context with pagination

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,11 @@
 
 # app/controllers/tasks_controller.rb
 class TasksController < ApplicationController
-  # after_action { pagy_headers_merge(@pagy) if @pagy }
+  after_action { pagy_headers_merge(@pagy) if @pagy }
 
   def index
-    pagy, tasks = pagy(Task.all, page: params[:page], items: params[:items]) # rubocop:disable Lint/UselessAssignment
+    @pagy, tasks = pagy(Task.all, page: params[:page], items: params[:items])
+    pagy_headers_merge(@pagy)
 
     render json: tasks,
            each_serializer: Tasks::Index::TasksSerializer,
