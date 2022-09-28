8fcdcdd620c65d72bb4c9f22089cb46b01f0d030
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-09-27 22:11:12 -0300

    add index text context with pagination

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -79,12 +79,10 @@
 # Headers extra: http response headers (and other helpers) useful for API pagination
 # See http://ddnexus.github.io/pagy/extras/headers
 require 'pagy/extras/headers'
-Pagy::DEFAULT[:headers] = {
-                            page: 'Current-Page',
+Pagy::DEFAULT[:headers] = { page: 'Current-Page',
                             items: 'Page-Items',
                             count: 'Total-Count',
-                            pages: 'Total-Pages'
-                          } # default
+                            pages: 'Total-Pages' } # default
 
 # Meilisearch extra: Paginate `Meilisearch` result objects
 # See https://ddnexus.github.io/pagy/extras/meilisearch
