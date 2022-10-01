2b98aa3e38edd10e0e65bb37ce587b743be0bbcd
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-10-01 18:33:20 -0300

    apply some corrections in schema.rb
    the corrections applyed are based in rubocop errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to have_many(:tasks).dependent(:destroy) }
+  context 'associations' do
+    it { is_expected.to have_many(:tasks).dependent(:destroy) }
+  end
 end
