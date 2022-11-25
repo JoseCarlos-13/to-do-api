7c31f4244cce2f9706e8cc621c0866a1e6ab0523
JoseCarlos-13 <josecarlosbsilva.17@gmail.com>
2022-11-25 18:34:17 -0300

    add the scope filter for title and user name

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,12 @@ class TasksController < ApplicationController
   after_action { pagy_headers_merge(@pagy) if @pagy }
 
   def index # rubocop:disable Metrics/BlockLength
-    if params[:status] && params[:title]
-      task_list = Task.all.includes(:user).filter_by_status(params[:status], params[:title])
+    if params[:status].present?
+      task_list = Task.all.includes(:user).filter_by_status(params[:status])
+    elsif params[:title].present?
+      task_list = Task.all.includes(:user).filter_by_title(params[:title])
+    elsif params[:user_name].present?
+      task_list = Task.all.includes(:user).filter_by_user_name(params[:user_name])
     else
       task_list = Task.all.includes(:user)
     end
--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,7 @@ class Task < ApplicationRecord
 
   enum status: { to_do: 0, in_development: 1, done: 2, cancelled: 3 }
 
-  scope :filter_by_status, ->(status_params) {
-    where("status = :status and title = :title", { status: statuses[status_params], title: "%#{title}%" })
-  }
+  scope :filter_by_status, ->(status) { where(status: statuses[status]) }
+  scope :filter_by_title, ->(title) { where('title like ?', "%#{title}%") }
+  scope :filter_by_user_name, ->(user_name) { self.joins(:user).where("name like ?", "%#{user_name}%") }
 end
